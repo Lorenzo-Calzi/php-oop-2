7b0ae36a06da033998568fa1c9b7d4d27fd96b78
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-28 15:46:12 +0200

    Corretta funzione Sconto

--- index.php
+++ index.php
@@ -51,48 +51,61 @@ class User
     public $lastname;
     public $nickname;
     public $email;
-    public $eta;
     public $sconto = 0;
 
-    function setSconto($eta) {
-        if($eta > 40) {
-            $this->sconto = 10;
-        }
-    }
-
-    function __construct(string $name, string $lastname, string $nickname, string $email, int $eta)
+    function __construct(string $name, string $lastname, string $nickname, string $email)
     {
         $this->name = $name;
         $this->lastname = $lastname;
         $this->nickname = $nickname;
         $this->email = $email;
-        $this->eta = $eta;
+        /* $this->eta = $eta; */
     } 
+
+    public function setSconto($eta) {
+        if($eta > 40) {
+            $this->sconto = 10;
+        }
+    }
 }
 
 /* Premium Extends */
 class Premium extends User
 {
-    public $discount;
+    public $level;
 
-    function __construct(string $name, string $lastname, string $nickname, string $email, int $eta, int $discount)
+    function __construct(string $name, string $lastname, string $nickname, string $email, int $level)
     {
-        parent::__construct($name, $lastname, $nickname, $email, $eta);
-        $this->discount = $discount;
+        parent::__construct($name, $lastname, $nickname, $email);
+        $this->level = $level;
+    }
+
+    public function setSconto($eta) {
+        if($eta > 40) {
+            $this->sconto = 20;
+        } else{
+            $this->sconto = $this->level *10;
+        }
     }
 }
 
 
 
-$chiara = new User('Chiara', 'Liporata', 'Lipo', 'chiara@example.com', 19);
-$edoardo = new User('Edoardo', 'Strada', 'Dodo', 'edo@example.com', 41);
+$chiara = new User('Chiara', 'Liporata', 'Lipo', 'chiara@example.com');
+$chiara->setSconto(19);
 var_dump($chiara);
+
+$edoardo = new User('Edoardo', 'Strada', 'Dodo', 'edo@example.com');
+$edoardo->setSconto(41);
 var_dump($edoardo);
 
 
-$tiziano = new Premium('Tiziano', 'Amati', 'Tizi', 'tizi@example.com', 52, 15);
-$lorenzo = new Premium('Lorenzo', 'Calzi', 'Lo', 'lorenzo@example.com', 21, 40);
+$tiziano = new Premium('Tiziano', 'Amati', 'Tizi', 'tizi@example.com', 1);
+$tiziano->setSconto(52);
 var_dump($tiziano);
+
+$lorenzo = new Premium('Lorenzo', 'Calzi', 'Lo', 'lorenzo@example.com', 3);
+$lorenzo->setSconto(21);
 var_dump($lorenzo);
 
 /* /USER */
